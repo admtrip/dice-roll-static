e2bec5c12bece5bd87a4ed15b398b93b660eb116
admtrip <atripat0@chicagobooth.edu>
2025-02-11 06:02:42 +0000

    finished some links

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 get("/dice/2/6", { :controller => "dice", :action => "two_six" })
 
 get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
+
+get("/dice/1/20", { :controller => "dice", :action => "two_ten" })
+
+get("/", { :controller => "dice", :action => "homepage" })
+
+
 end
